1ec7c74485dcb273058b30eae698d4c84cd21ae7
Ulf Bourelius <ulf.bourelius@edu.newton.se>
2026-01-09 12:54:26 +0100

    added test project

---
+++ tests/QuizBattle.Tests/QuestionTests.cs
@@ -0,0 +1,20 @@
+﻿using QuizBattle.Domain;
+
+namespace QuizBattle.Tests;
+
+public class QuestionTests
+{
+    [Fact]
+    public void QuestionText_IsNull_ThrowsDomainException()
+    {
+        try
+        {
+            var question = new Question(null!, null!, null!);
+            Assert.Fail("Expected to throw DomainException with null parameters.");
+        }
+        catch(DomainException ex)
+        {
+            Console.WriteLine("Correct exception: ", ex.Message);
+        }
+    }
+}
