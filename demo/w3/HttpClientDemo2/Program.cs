a1765176bf4f843595c6f20ad38e3da36abf0aae
ulfnewton <ulf.bourelius@edu.newton.se>
2026-01-13 13:48:21 +0100

    added http client demo 2

---
+++ demo/w3/HttpClientDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// 1. Registrera en namngiven klient
+// Vi sätter bas-inställningarna här så vi slipper upprepa dem.
+builder.Services.AddHttpClient("GitHubClient", client =>
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/posts");
+    // GitHub KRAV: En User-Agent måste finnas, annars får du 403 Forbidden.
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("MinEnklaDemoApp");
+});
+
+var app = builder.Build();
+
+// 2. Injicera IHttpClientFactory direkt i metoden
+app.MapGet("/", async (IHttpClientFactory factory) =>
+{
+    // Skapa klienten baserat på namnet vi angav ovan
+    var client = factory.CreateClient("placeholder");
+
+    // Gör anropet (hämtar rå JSON-text eftersom vi inte har någon DTO)
+
+    
+
+    string json = await client.GetStringAsync("/posts");
+
+    return json;
+});
+
+app.Run();
